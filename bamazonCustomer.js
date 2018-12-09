f163f72af2e313e4a84f37008daf6d5f7aac50aa
dancrump1 <42187710+dancrump1@users.noreply.github.com>
2018-12-08 21:17:44 -0500

    customer purchase works. need to update sql

---
+++ bamazonCustomer.js
@@ -0,0 +1,42 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user:"root",
+    password:"12345678",
+    database: "bamazon_db"
+});
+
+function startBam(){
+    connection.query(`SELECT * FROM products`, function(err, res){
+        if(err)throw err;
+        console.log(res);
+        inquirer.prompt([
+            {
+                message: "Please enter the item ID number that you wish to purchase.",
+                name: "productID"
+            },
+            {
+               message:"How many units would you like to purchase?",
+               name:"numberOfUnits"
+            }
+        ]).then( function(res){
+            connection.query("SELECT stock_quantity FROM products WHERE item_ID=?", [res.productID], function(err, response){
+                if (err) throw err;
+                console.log(`There are currently ${response[0].stock_quantity} in stock.`);
+                console.log(`You've requested ${res.numberOfUnits} units.`);
+                if(response[0].stock_quantity > res.numberOfUnits){
+                    connection.query(`UPDATE products set stock_quantity = (?-?) WHERE item_id = ?`,[response[0].stock_quantity, res.productID, res.numberOfUnits]);
+                    console.log("Purchase complete!")
+                }
+
+            });
+        });
+    
+        
+    });
+}
+
+startBam();
